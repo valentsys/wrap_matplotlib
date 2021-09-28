b0f829bc55c51dfeeb7cc96930491087c2b425f7
valentsys <35652878+valentsys@users.noreply.github.com>
2021-09-28 14:37:38 +0300

    plot engine initial commit
    A very early unpolished version of the script.

--- plot_engine.py
+++ plot_engine.py
@@ -1,5 +1,24 @@
-from .config import *
-from .data_funcs import *
+"""
+Copyright (c) 2021 Vladislav Ivanov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 
 import math
 import matplotlib as mpl
@@ -12,6 +31,8 @@ import matplotlib.ticker as ticky
 import matplotlib.colors as mcolors
 
 import numpy as np
+
+maxfontsize= 22
 
 #HELPERS
 def preserve_eps_text():  
